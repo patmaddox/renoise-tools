3436cb5cd24b03a33b63be504d6298e7f20d1965
Pat Maddox <pat@rubysteps.com>
2015-08-24 16:14:09 -0600

    fixed the bug

--- com.patmaddox.TurnItDown.xrnx/main.lua
+++ com.patmaddox.TurnItDown.xrnx/main.lua
@@ -4,22 +4,41 @@
 -- local options = renoise.Document.create("TurnItDown") { }
 -- renoise.tool().preferences = options
 
-local function attenuate_samples(context)
+local function attenuate_sample(sample)
+  local attenuation = 0.25118863582611; -- -12.00 dB
+  sample.volume = attenuation;
+end
+
+local function on_samples_changed(instrument, context)
+  rprint(context);
   if context and context.type == "insert" then
-    local attenuation = 0.25118863582611; -- -12.00 dB
-    renoise.song().selected_instrument:sample(context.index).volume = attenuation;
+    local sample = instrument:sample(context.index);
+    attenuate_sample(sample);
   end
 end
 
-local function on_instrument_selected()
-  if not renoise.song().selected_instrument.samples_observable:has_notifier(attenuate_samples) then
-    renoise.song().selected_instrument.samples_observable:add_notifier(attenuate_samples);
+local function add_on_samples(instrument)
+  if not instrument.samples_observable:has_notifier(on_samples_changed) then
+    instrument.samples_observable:add_notifier(instrument, on_samples_changed);
+  end
+end
+
+local function on_instrument_added(context)
+  if context and context.type == "insert" then
+    local instrument = renoise.song():instrument(context.index);
+    add_on_samples(instrument);
+
+    for i, sample in ipairs(instrument.samples) do
+      attenuate_sample(sample);
+    end
   end
 end
 
 local function on_song_created()
-  renoise.song().selected_instrument_observable:add_notifier(on_instrument_selected);
-  on_instrument_selected(); -- for the first instrument
+  renoise.song().instruments_observable:add_notifier(on_instrument_added);
+  for i, instrument in ipairs(renoise.song().instruments) do
+    add_on_samples(instrument);
+  end
 end
 
 -- add new song observer
